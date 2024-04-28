013a1795582b06c40f4c968e0a2aba6b10a933c0
Aleksei Filippov <filippov@MacBook-Pro-Aleksei.local>
2024-04-28 13:48:17 +0300

    7 commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import concurrent.futures
 import speedtest
 
 
-
+print(time.time())
 time.sleep(7)
 
 
@@ -17,6 +17,7 @@ def get_exchange_tickers(exchange_url, name):
         return [ticker['symbol'] for ticker in tickers['data']]
     if name == 'mexc':
         return [ticker['symbol'].replace('_', '') for ticker in tickers['data']]
+
 
 print(time.time())
 
@@ -38,30 +39,48 @@ tickers = list(set(binance_spot_tickers) & set(bitget_futures_tickers + mexc_fut
 print(len(tickers))
 
 
+def speedtester():
+    print("speedtest")
+    st = speedtest.Speedtest()
+    st.get_best_server()
+    download_speed = st.download()
+    upload_speed = st.upload()
+    ping = st.results.ping
+    download_mbps = round(download_speed / 1_000_000, 2)
+    upload_mbps = round(upload_speed / 1_000_000, 2)
+    print(f"Скорость загрузки: {download_mbps} Мбит/с")
+    print(f"Скорость отдачи: {upload_mbps} Мбит/с")
+    print(f"Пинг: {ping} мс")
+
+
 def get_order_book(symbol):
     url = f"https://api.binance.com/api/v3/depth?symbol={symbol}&limit=500"
-    try:
-        response = requests.get(url, timeout=10)
-    except:
-        response = requests.get(url, timeout=10)
+    fail = True
+    while fail:
+        try:
+            response = requests.get(url, timeout=10)
+            fail = False
+        except:
+            print("except: ", url)
     data = response.json()
     return data
 
 
 def calculate_average_volume(symbol):
     candles_url = f"https://api.binance.com/api/v3/klines?symbol={symbol}&interval=15m&limit=11"
-    try:
-        candles_response = requests.get(candles_url, timeout=10)
-    except:
-        candles_response = requests.get(candles_url, timeout=10)
-    #print(candles_response)
+    fail = True
+    while fail:
+        try:
+            candles_response = requests.get(candles_url, timeout=10)
+            fail = False
+        except:
+            print("except: ", candles_url)
     candles_data = candles_response.json()
-    #print(candles_data)
     sum_volume = 0
     for i in range(len(candles_data) - 1):
         sum_volume += float(candles_data[i][5])
     volume = sum_volume / 150
-    return volume
+    return volume * 2
 
 
 def find_large_orders(symbol):
@@ -72,13 +91,12 @@ def find_large_orders(symbol):
     average_volume = calculate_average_volume(symbol)
 
     large_orders = []
-    #print(order_book)
     if len(order_book['bids']) == 0 and len(order_book['bids']) == 0:
         return large_orders
 
     curr_price = float(order_book['bids'][0][0])
     for bid in order_book['bids']:
-        if float(bid[1]) > average_volume * 10 and float(bid[1]) * float(bid[0]) >= 100000:
+        if float(bid[1]) > average_volume * 15 and float(bid[1]) * float(bid[0]) >= 100000:
             large_orders.append({
                 'ticker': symbol,
                 'side': 'BUY',
@@ -91,7 +109,7 @@ def find_large_orders(symbol):
 
     curr_price = float(order_book['asks'][0][0])
     for ask in order_book['asks']:
-        if float(ask[1]) > average_volume * 10 and float(ask[1]) * float(ask[0]) >= 100000:
+        if float(ask[1]) > average_volume * 15 and float(ask[1]) * float(ask[0]) >= 100000:
             large_orders.append({
                 'ticker': symbol,
                 'side': 'SELL',
@@ -111,33 +129,25 @@ def update_large_orders(temp_all_large_orders, all_large_orders, n):
     else:
         for i in range(len(temp_all_large_orders)):
             if len(temp_all_large_orders[i]) != 0:
-                #print("upd_s: ", temp_all_large_orders[i], i)
                 ticker_price = []
                 del_list = []
                 for j in range(len(all_large_orders[i])):
-                    #print(all_large_orders[i])
                     ticker_price.append(float(all_large_orders[i][j]['price']))
                 if len(all_large_orders[i]) != 0:
                     for j in range(len(all_large_orders[i])):
                         is_exist = False
                         for k in range(len(temp_all_large_orders[i])):
                             if float(temp_all_large_orders[i][k]['price']) == float(all_large_orders[i][j]['price']) and temp_all_large_orders[i][k]['side'] == all_large_orders[i][j]['side']:
-                                #print(" in 3 for: ", all_large_orders[i][j], temp_all_large_orders[i][k])
-                                #print("next: ", len(all_large_orders[i]), len(temp_all_large_orders[i]))
                                 time_find = all_large_orders[i][j]['time_find']
                                 all_large_orders[i][j] = temp_all_large_orders[i][k]
                                 all_large_orders[i][j]['time_find'] = time_find
                                 is_exist = True
                             elif float(temp_all_large_orders[i][k]['price']) not in ticker_price:
-                                #print(" in 4 for: ", all_large_orders[i][j], temp_all_large_orders[i][k])
-                                #print("next: ", len(all_large_orders[i]), len(temp_all_large_orders[i]))
                                 all_large_orders[i].append(temp_all_large_orders[i][k])
                                 ticker_price.append(float(temp_all_large_orders[i][k]['price']))
                                 is_exist = True
                                 break
                             elif float(temp_all_large_orders[i][k]['price']) == float(all_large_orders[i][j]['price']) and temp_all_large_orders[i][k]['side'] != all_large_orders[i][j]['side']:
-                                #print(" in 5 for: ", all_large_orders[i][j], temp_all_large_orders[i][k])
-                                #print("next: ", len(all_large_orders[i]), len(temp_all_large_orders[i]))
                                 all_large_orders[i].append(temp_all_large_orders[i][k])
                                 is_exist = False
                                 break
@@ -146,12 +156,10 @@ def update_large_orders(temp_all_large_orders, all_large_orders, n):
                     del_list.reverse()
                     for j in range(len(del_list)):
                         all_large_orders[i].pop(del_list[j])
-                    #print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! all_large_orders: ", all_large_orders[i])
                 else:
                     for k in range(len(temp_all_large_orders[i])):
                         all_large_orders[i].append(temp_all_large_orders[i][k])
             else:
-                #print("!!!!!!!!!!!!!!!!!!!!!! temp_all_large_orders: ", i, temp_all_large_orders[i])
                 if len(all_large_orders) > 0:
                     for j in range(len(all_large_orders[i])):
                         all_large_orders[i].pop(0)
@@ -163,7 +171,7 @@ def alert(all_large_orders):
         for j in range(len(all_large_orders[i])):
             ticker = all_large_orders[i][j]['ticker']
             duration = (time.time() - all_large_orders[i][j]["time_find"]) / 60
-            if abs(all_large_orders[i][j]['diff']) < 1.5 and duration > 1 and ticker != "USDCUSDT":
+            if abs(all_large_orders[i][j]['diff']) < 1 and duration > 1 and ticker != "USDCUSDT":
                 for client, tickers in clients.items():
                     if ticker not in tickers.keys() or time.time() - tickers[ticker] > 1800:
                         if ticker == 'BCHUSDT':
@@ -220,9 +228,7 @@ duration: {round(duration, 2)}m
                         bot.send_message(client, str_msg, parse_mode="Markdown")
                         clients[client][ticker] = time.time()
                         print("client: ", clients)
-                        time.sleep(1)
-                        print("ticker: ", time.time() - tickers[ticker])
-
+                        time.sleep(0.5)
 
 
 secret_key = b"2b51734631bb4c0fa540a20022637fb8"
@@ -238,45 +244,18 @@ def start_message(message):
 
 
 def process():
-
     all_large_orders = []
     n = 0
     while True:
-        print("speedtest")
-        st = speedtest.Speedtest()
-        st.get_best_server()
-        download_speed = st.download()
-        upload_speed = st.upload()
-        ping = st.results.ping
-        download_mbps = round(download_speed / 1_000_000, 2)
-        upload_mbps = round(upload_speed / 1_000_000, 2)
-        print(f"Скорость загрузки: {download_mbps} Мбит/с")
-        print(f"Скорость отдачи: {upload_mbps} Мбит/с")
-        print(f"Пинг: {ping} мс")
-
+        speedtester()
         temp_all_large_orders = []
         with concurrent.futures.ThreadPoolExecutor() as executor:
             temp_all_large_orders = list(executor.map(find_large_orders, tickers))
         time.sleep(2)
-
-        response = requests.get('https://api.binance.com/api/v3/time')
-        if response.status_code == 200:
-            print("Used weight (1 minute):", response.headers.get('X-MBX-USED-WEIGHT-1M'))
-            print("Order count (10 seconds):", response.headers.get('X-MBX-ORDER-COUNT-10S'))
-            print("Order count (1 day):", response.headers.get('X-MBX-ORDER-COUNT-1D'))
-        else:
-            print("Failed to fetch data:", response.status_code)
-
-        # for ticker in tickers:
-        #     large_orders = find_large_orders(ticker)
-        #     temp_all_large_orders.append(large_orders)
-        #     #print("all: ", all_large_orders)
-        #     print(time.time(), ticker)
         all_large_orders = update_large_orders(temp_all_large_orders, all_large_orders, n)
         if n > 0:
             alert(all_large_orders)
         n += 1
-
 
 
 Thread(target=process).start()
